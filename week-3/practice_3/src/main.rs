426373910a6947265f7d03ccfb8e53b14da268bb
Chiedulaura <chiedulaura200@gmail.com>
2022-10-28 20:58:10 +0100

    (My first commit)

---
+++ week-3/practice_3/src/main.rs
@@ -0,0 +1,10 @@
+fn main () {
+    let results = 10.00; //f64 by default
+    let interest:f32 = 8.35;
+    let cost:f64 = 15000.600; //double precision
+
+
+    println!("result value is {}",results);
+    println!("interest is {}",interest);
+    println!("cost is {}",cost);
+}
