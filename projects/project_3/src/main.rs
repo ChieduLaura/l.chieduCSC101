c9f790a76667d7d526689de2978bba23e9b4f542
Chiedulaura <chiedulaura200@gmail.com>
2023-01-15 20:09:10 +0100

    (project 3)

--- projects/project_3/src/main.rs
+++ projects/project_3/src/main.rs
@@ -22,56 +22,7 @@ fn main() {
         io::stdin().read_line(&mut answer).expect("Invalid input");
         let function: f64 = answer.trim().parse().expect("Invalid input");
 
-        if function == 1;
-        {
-            let mut input1 = String::new(); 
-            println!("How many portions of Poundo Yam/ Edinkaiko Soup would you like?");
-            io::stdin().read_line(&mut input1).expect("Invalid input");
-            let a :i32 = input1.trim().parse().expect("Invalid input");
-        }
-
-       else if function == 2;
-        {
-        let mut input2 = String::new();
-        println!("How many portions of Fried Rice & Chicken would you like? ");
-        io::stdin().read_line(&mut input2).expect("Invalid input");
-        let a: i32 = input2.trim().parse().expect("Invalid input");
-    }
-    else if function == 3;
-    {
-        let mut input3 = String::new();
-        println!("How many portions of Amala & Ewedu Soup would you like? ");
-        io::stdin().read_line(&mut input3).expect("Invalid input");
-        let a: i32 = input3.trim().parse().expect("Invalid input");
-    }
-    else if function == 4;
-    {
-        let mut input4 = String::new();
-        println!("How many portions of Eba & Egusi Soup would you like? ");
-        io::stdin().read_line(&mut input4).expect("Invalid input");
-        let a: i32 = input4.trim().parse().expect("Invalid input");
-    }
-    else if function == 5;
-    {
-        let mut input5 = String::new();
-        println!("How many portions of White Rice & Stew would you like? ");
-        io::stdin().read_line(&mut input5).expect("Invalid input");
-        let a: i32 = input5.trim().parse().expect("Invalid input");
-    }
-
-            let mut seconds = String::new();
-            println!("Would you like to order anything else?");
-            io::stdin().read_line(&mut seconds).expect("Invalid input");
-
-            if seconds = yes;
-            {
-              let mut answer = String::new();
-        println!("Which would you like to eat today?");
-        io::stdin().read_line(&mut answer).expect("Invalid input");
-        let function: f64 = answer.trim().parse().expect("Invalid input");
-                }
-        if function == 1.0
-        {
+        if function == 1.0 {
             let mut input1 = String::new(); 
             println!("How many portions of Poundo Yam/ Edinkaiko Soup would you like?");
             io::stdin().read_line(&mut input1).expect("Invalid input");
@@ -83,31 +34,30 @@ fn main() {
         let mut input2 = String::new();
         println!("How many portions of Fried Rice & Chicken would you like? ");
         io::stdin().read_line(&mut input2).expect("Invalid input");
-        let a: i32 = input2.trim().parse().expect("Invalid input");
+        let _a: i32 = input2.trim().parse().expect("Invalid input");
     }
     else if function == 3.0
     {
         let mut input3 = String::new();
         println!("How many portions of Amala & Ewedu Soup would you like? ");
         io::stdin().read_line(&mut input3).expect("Invalid input");
-        let a: i32 = input3.trim().parse().expect("Invalid input");
+        let _a: i32 = input3.trim().parse().expect("Invalid input");
     }
-    else if function == 4.0ssss   {
+    else if function == 4.0
+    {
         let mut input4 = String::new();
         println!("How many portions of Eba & Egusi Soup would you like? ");
         io::stdin().read_line(&mut input4).expect("Invalid input");
-        let a: i32 = input4.trim().parse().expect("Invalid input");
+        let _a: i32 = input4.trim().parse().expect("Invalid input");
     }
-    else if function == 5;
+    else if function == 5.0
     {
         let mut input5 = String::new();
         println!("How many portions of White Rice & Stew would you like? ");
         io::stdin().read_line(&mut input5).expect("Invalid input");
-        let a: i32 = input5.trim().parse().expect("Invalid input");
+        let _a: i32 = input5.trim().parse().expect("Invalid input");
     }
-            else if seconds = no;
-            {
-            println!("Thank you for eating here!")
-        }
+
+           println!("Thank you for eating here!");
 
    }
